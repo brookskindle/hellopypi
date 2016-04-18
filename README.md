11cd66d4b21d3004cb445513366443c6704e2061
Brooks Kindle <Brooks.Kindle@emc.com>
2016-04-18 13:05:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Small package to test the functionality of uploading to pypi.
+
+Sample output (after installation)
+```python
+>>> import hellopypi
+Hello, Pypi!
+```
+
+Works in python 2 and python 3.
+
+> Note: there is a hellopypi package on the official pypi site. This is **not**
+> that project. This is a small project I created to test upload of a project
+> to the pypi test server.
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+#from distutils.core import setup
+
+setup(
+    name="HelloPyPi",
+    packages=["hellopypi"],
+    version="0.0.1dev",
+    description="A test package upload.",
+    long_description=("Importing the 'hellopypi' module will cause a greeting "
+                      "message to be displayed to stdout."),
+    author="Brooks Kindle",
+    author_email="brookskindle@gmail.com",
+    url="https://github.com/brookskindle/hellopypi",
+    keywords=["test", "hello"]
+)
